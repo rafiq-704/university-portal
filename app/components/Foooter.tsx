e17c4f66d8b8a019cbae908f0e1c0713089faa68
JavedAkhtar1220 <j.akhtar1220@gmail.com>
2026-01-05 18:35:32 +0500

    update footer text

--- app/components/Foooter.tsx
+++ app/components/Foooter.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
     <footer className="w-full bg-primary ">
       <div className="flex flex-col items-center justify-center gap-10">
         <div className="email flex items-center justify-center py-6 text-xl">
-          <a href="mailto:careerday@bsse.ethz.ch" className="hover:underline">careerday@bsse.ethz.ch</a>
+          Contact us:<a href="mailto:careerday@bsse.ethz.ch" className="hover:underline">careerday@bsse.ethz.ch</a>
         </div>
         {/* <div className="email flex items-center justify-end w-full text-xl p-4">
           <a href="mailto:careerday@bsse.ethz.ch">Website By: </a>
